f04bfe27a9871328599f71003f152495bfa46a63
camile <camile.royer22@gmail.com>
2026-09-03 17:45:27 -0300

    branch finalizada
    finalizei os arquivos Item, Main e CD.. agora só fazer o resto e finalizar o app e os outros arquivos

---
+++ App.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class App {
+    private Scanner entrada;
+    private ArrayList<Dvd> listaDvd;
+
+    public App() {
+        entrada = new Scanner(System.in); 
+    }
+
+    public void executar() {
+        int opcao;
+        do {
+            System.out.println("=====================");
+            System.out.println("SISTEMA DE ITENS COLECIONAVEIS");
+            menu();
+            System.out.print("Digite a opcao desejada: ");
+            opcao = entrada.nextInt();
+            entrada.nextLine();
+            switch (opcao) {
+                case 0:
+                    break;
+                case 1:
+                    //cadastrarLivro();
+                    break;
+                case 2:
+                    //cadastrarCd();
+                    break;
+                case 3:
+                    cadastrarDvd();
+                    break;
+                case 4:
+                    //cadastrarRevista();
+                    break;
+                case 5:
+                    //pesquisarItemEspecifico();
+                case 6:
+                    //pesquisarID()
+                default:
+                    System.out.println("=====================");
+                    System.out.println("Opcao invalida. Redigite, por favor.");
+            }
+        } while (opcao != 0);
+    }
+
+    private void menu() {
+        System.out.println("Opcoes: ");
+        System.out.println("[0] Sair");
+        System.out.println("[1] Cadastrar livro");
+        System.out.println("[2] Cadastrar CD");
+        System.out.println("[3] Cadastrar DVD");
+        System.out.println("[4] Cadastrar revista");
+        System.out.println("[5] Pesquisar por item específico");
+        System.out.println("[6] Pesquisar por ID");
+    }
+
+    private void cadastrarDvd() {
+        System.out.println("=====================");
+        System.out.println("Cadastrar um DVD: ");
+        System.out.println("Digite o ID: ");
+        int id = entrada.nextInt();
+        entrada.nextLine();
+        System.out.print("Digite o nome: ");
+        String nome = entrada.nextLine();
+        System.out.print("Digite a data de aquisição: ");
+        int dataAquisicao = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Digite o tipo: ");
+        String tipo = entrada.nextLine();
+        System.out.println("Digite a descrição do DVD: ");
+        String descricao = entrada.nextLine();
+        Dvd novoDvd = new Dvd(id, nome, dataAquisicao, tipo, descricao);
+    
+    System.out.println("\n Cadastro dos Autores: ");
+    while (true) {
+        System.out.print("Digite o nome de um autor (ENTER para finalizar): ");
+        String autor = entrada.nextLine();
+        if (autor.trim().isEmpty()) { //evitar espaços brancos e rodar
+            break;
+        }
+        novoDvd.adicionarAutor(autor);
+    }
+    listaDvd.add(novoDvd);
+    System.out.println("=====================");
+    System.out.println("DVD cadastrado com sucesso!");
+    }
+}
--- Item.java
+++ Item.java
@@ -1,5 +1,26 @@
 //identificação única, nome, data de aquisição, lista de autores.
 
-public class Item{
+import java.util.ArrayList;
 
+public abstract class Item{
+    private int id;
+    private String nome;
+    private int dataAquisicao;
+    private ArrayList<String> autores;
+
+    public Item(int id, String nome, int dataAquisicao){
+        this.id = id;
+        this.nome = nome;
+        this.dataAquisicao = dataAquisicao;
+        autores = new ArrayList<String>();
+    }
+
+    public int getId(){ return id; }
+    public String getNome() {return nome; }
+    public int getDataAquisicao(){ return dataAquisicao; }
+    public ArrayList<String> getAutores() { return autores; }
+
+     public void adicionarAutor(String autor) {
+            this.autores.add(autor);
+    }
 }
